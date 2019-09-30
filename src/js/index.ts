ed58c65d4336df8357a9f6f4191d1ac2e0e7ae48
Anders Børjesson <anbo@zealand.dk>
2019-09-30 10:42:45 +0200

    fixed axios import

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,7 +1,7 @@
 import axios, {
     AxiosResponse,
     AxiosError
-} from "../../node_modules/axios"; // Don't worry about red lines here ...
+} from "../../node_modules/axios/index"; // Don't worry about red lines here ...
 
 interface IPost {
     // attributes from http://jsonplaceholder.typicode.com/posts
@@ -13,8 +13,8 @@ interface IPost {
 
 let element: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
 
-axios.get<IPost>("http://jsonplaceholder.typicode.com/posts")
-    .then((response: AxiosResponse<IPost>) => {
+axios.get<IPost[]>("http://jsonplaceholder.typicode.com/posts")
+    .then((response: AxiosResponse<IPost[]>) => {
         let data: IPost[] = response.data;
         element.innerHTML = JSON.stringify(data);
         data.forEach(element => {
@@ -23,4 +23,5 @@ axios.get<IPost>("http://jsonplaceholder.typicode.com/posts")
     })
     .catch((error: AxiosError) => {
         console.log(error);
+        element.innerHTML = error.message;
     });
