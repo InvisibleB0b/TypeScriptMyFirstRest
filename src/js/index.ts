8268c20fa5da0516e1c76cd0db6df5bc9d99ff65
Anders Børjesson <anbo@zealand.dk>
2020-03-05 20:42:41 +0100

    extracted method

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,7 +1,7 @@
 import axios, {
     AxiosResponse,
     AxiosError
-} from "../../node_modules/axios/index"; 
+} from "../../node_modules/axios/index";
 
 interface IPost {
     // attributes from http://jsonplaceholder.typicode.com/posts
@@ -17,12 +17,7 @@ axios.get<IPost[]>("http://jsonplaceholder.typicode.com/posts")
     .then((response: AxiosResponse<IPost[]>) => {
         let data: IPost[] = response.data;
         //element.innerHTML = JSON.stringify(data);
-        let longHtml: string = "<ol>";
-        data.forEach(element => {
-            console.log(element.title);
-            longHtml += "<li>" + element.title + "</li>";
-        });
-        longHtml += "</ol>";
+        let longHtml: string = makeLongHtmlString(data);
         element.innerHTML = longHtml;
     })
     .catch((error: AxiosError) => {
@@ -32,3 +27,13 @@ axios.get<IPost[]>("http://jsonplaceholder.typicode.com/posts")
 
 let messageElement: HTMLDivElement = <HTMLDivElement>document.getElementById("message");
 messageElement.innerHTML = "Done";
+
+function makeLongHtmlString(data: IPost[]) {
+    let longHtml: string = "<ol>";
+    data.forEach(element => {
+        console.log(element.title);
+        longHtml += "<li>" + element.title + "</li>";
+    });
+    longHtml += "</ol>";
+    return longHtml;
+}
